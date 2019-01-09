2f951fb011dae1ff35fc647aa4a5a44812a88740
Shivansh Rai <shivansh@freebsd.org>
2019-01-10 00:30:22 +0530

    svm: add assignment 2 (accuracy=0.99)

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python
-
 """
     This is the code to accompany the Lesson 1 (Naive Bayes) mini-project.
 
@@ -15,23 +14,24 @@ from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
-
 
 #########################################################
 
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
+
+# Observations
+# ~~~~~~~~~~~~
+# training time: 1.015 s
+# prediction time: 0.123 s
+# accuracy: 0.9732650739476678
 
 clf = GaussianNB()
 t0 = time()
@@ -44,3 +44,10 @@ print "prediction time:", round(time() - t0, 3), "s"
 
 accuracy = accuracy_score(pred, labels_test)
 print "accuracy:", accuracy
+
+class_1_count = 0
+for p in pred:
+    if p == 1:
+        class_1_count += 1
+
+print "Events in Chris(1) class:", class_1_count
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -1,30 +1,60 @@
 #!/usr/bin/python
-
-""" 
+"""
     This is the code to accompany the Lesson 2 (SVM) mini-project.
 
-    Use a SVM to identify emails from the Enron corpus by their authors:    
+    Use a SVM to identify emails from the Enron corpus by their authors:
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
 
 #########################################################
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
+# Observations
+# ~~~~~~~~~~~~
+# Reducing the size of the features and labels to 1% reduces the accuracy to
+# 88 % from 98 % (for a linear kernel).
+#
+# Accuracies with different values of C -
+# C=10:    0.61
+# C=100:   0.61
+# C=1000:  0.82
+# C=10000: 0.89
+#
+# Recordings with (kernel='rbf', C=10000) -
+#   training time: 84.527 s
+#   prediction time: 8.544 s
+#   accuracy: 0.9908987485779295
 
+clf = svm.SVC(kernel='rbf', C=10000)
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time() - t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time:", round(time() - t0, 3), "s"
+
+accuracy = accuracy_score(pred, labels_test)
+print "accuracy:", accuracy
+
+class_1_count = 0
+for p in pred:
+    if p == 1:
+        class_1_count += 1
+
+print "Events in Chris(1) class:", class_1_count
